cd31f67a0b4e7d0d6537dd41d59545e883e295e8
Chetan Padia <chetbox@users.noreply.github.com>
2021-01-14 18:57:17 +0000

    workaround for Gatsby users

--- src/helpers/urls.ts
+++ src/helpers/urls.ts
@@ -1,5 +1,6 @@
 const COGS_SERVER_PORT = 12095;
 
 export function assetSrc(file: string): string {
-  return `${location.protocol}//${location.hostname}:${COGS_SERVER_PORT}/assets/${file}`;
+  const location = typeof window !== 'undefined' ? window.location : undefined;
+  return `${location?.protocol}//${location?.hostname}:${COGS_SERVER_PORT}/assets/${file}`;
 }
