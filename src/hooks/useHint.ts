a2f04b97f31edf263206a3d1a5bafabb2d4980b1
Chetan Padia <chetbox@users.noreply.github.com>
2021-04-16 09:14:43 +0100

    fix: add useCallback to useHint
    to prevent listener being detached

--- src/hooks/useHint.ts
+++ src/hooks/useHint.ts
@@ -1,17 +1,20 @@
 import { CogsConnection } from '@clockworkdog/cogs-client';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import useCogsMessage from './useCogsMessage';
 
 export default function useHint(connection: CogsConnection): string | null {
   const [hint, setHint] = useState('');
 
-  useCogsMessage(connection, (message) => {
-    switch (message.type) {
-      case 'text_hints_update':
-        setHint(message.lastSentHint);
-        break;
-    }
-  });
+  useCogsMessage(
+    connection,
+    useCallback((message) => {
+      switch (message.type) {
+        case 'text_hints_update':
+          setHint(message.lastSentHint);
+          break;
+      }
+    }, [])
+  );
 
   return hint || null;
 }
