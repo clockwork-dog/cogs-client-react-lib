0a5e5c869ca41b6fc59778d0be74acd08c72a599
Chetan Padia <chetbox@users.noreply.github.com>
2021-04-16 09:20:20 +0100

    add <Backgound> component

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
+// Utilities
 export { default as useIsConnected } from './hooks/useIsConnected';
-export { default as useAudioClips } from './hooks/useAudioClips';
-export { default as useIsAudioPlaying } from './hooks/useIsAudioPlaying';
-export { default as useBackground } from './hooks/useBackground';
 export { default as useCogsConfig } from './hooks/useCogsEvent';
 export { default as useCogsEvent } from './hooks/useCogsConfig';
 export { default as useCogsPortUpdate } from './hooks/useCogsPortUpdate';
 export { default as useCogsMessage } from './hooks/useCogsMessage';
+
+// Audio
+export { default as useAudioClips } from './hooks/useAudioClips';
+export { default as useIsAudioPlaying } from './hooks/useIsAudioPlaying';
+
+// Background
+export { default as useBackground } from './hooks/useBackground';
+export { default as Background } from './components/Background';
+
+// Hints
 export { default as useHint } from './hooks/useHint';
 export { default as Hint } from './components/Hint';
+
+// Timer
 export { default as Timer } from './components/Timer';
 export { default as Video } from './components/Video';
 export { assetSrc } from './helpers/urls';
