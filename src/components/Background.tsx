2f1777cf4227c0a695a8a6bbf0af1411b7a52558
Chetan Padia <chetbox@users.noreply.github.com>
2021-04-16 09:20:20 +0100

    add <Backgound> component

---
+++ src/components/Background.tsx
@@ -0,0 +1,36 @@
+import { CogsConnection } from '@clockworkdog/cogs-client';
+import React from 'react';
+import useBackground from '../hooks/useBackground';
+
+export default function Background({
+  className,
+  style,
+  connection,
+}: {
+  className?: string;
+  style?: React.CSSProperties;
+  connection: CogsConnection;
+}): JSX.Element {
+  const background = useBackground(connection);
+
+  return (
+    <div
+      {...{ className }}
+      style={{
+        zIndex: -1,
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: background?.color || 'transparent',
+        backgroundImage: background?.image?.file ? `url("${background.image.file}")` : 'none',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: background?.image?.fit ?? 'auto',
+        transition: 'filter 200ms',
+        ...style,
+      }}
+    />
+  );
+}
