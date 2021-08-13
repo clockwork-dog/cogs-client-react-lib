c0f564916f6d7045fcfbf7de7f38d59b00634d47
Chetan Padia <chetbox@users.noreply.github.com>
2021-08-13 12:27:16 +0100

    `useShowPhase` hook

---
+++ src/hooks/useShowPhase.ts
@@ -0,0 +1,18 @@
+import { CogsClientMessage, CogsConnection, ShowPhase } from '@clockworkdog/cogs-client';
+import { useCallback, useState } from 'react';
+import useCogsMessage from './useCogsMessage';
+
+export default function useShowPhase(connection: CogsConnection): ShowPhase {
+  const [status, setStatus] = useState<ShowPhase>(ShowPhase.Setup);
+
+  useCogsMessage(
+    connection,
+    useCallback((message: CogsClientMessage) => {
+      if (message.type === 'show_phase') {
+        setStatus(message.phase);
+      }
+    }, [])
+  );
+
+  return status;
+}
