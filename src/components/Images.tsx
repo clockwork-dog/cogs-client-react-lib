870a377a9fc156df43d7f9b6f876f3b943e0cce9
Chetan Padia <chetbox@users.noreply.github.com>
2021-08-18 12:04:07 +0100

    fix multiple image rendering

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -17,31 +17,29 @@ export default function Images({
 
   const fullscreenCustomStyle = typeof fullscreen === 'object' ? fullscreen.style : undefined;
 
+  const imageElements = images.map((image, index) => (
+    <img
+      className={className}
+      key={index}
+      src={assetUrl(image.file)}
+      alt={image.file}
+      style={{
+        objectFit: image.fit,
+        ...(fullscreen ? { width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 } : {}),
+        ...style,
+      }}
+    />
+  ));
+
   return (
     <>
-      {images.map((image, index) => {
-        const imageElement = (
-          <img
-            className={className}
-            key={index}
-            src={assetUrl(image.file)}
-            alt={image.file}
-            style={{
-              objectFit: image.fit,
-              ...(fullscreen ? { width: '100%', height: '100%' } : {}),
-              ...style,
-            }}
-          />
-        );
-
-        return fullscreen ? (
-          <div style={{ position: 'absolute', zIndex: 2, top: 0, left: 0, width: '100vw', height: '100vh', ...fullscreenCustomStyle }}>
-            {imageElement}
-          </div>
-        ) : (
-          imageElement
-        );
-      })}
+      {fullscreen ? (
+        <div style={{ position: 'absolute', zIndex: 2, top: 0, left: 0, width: '100vw', height: '100vh', ...fullscreenCustomStyle }}>
+          {imageElements}
+        </div>
+      ) : (
+        imageElements
+      )}
     </>
   );
 }
