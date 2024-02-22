8d10546bdea63f77b8bf88c045722fd83edf631b
Chetan Padia <chetbox@users.noreply.github.com>
2024-02-22 15:16:17 +0000

    Allow `null` values for hooks that may return `null`
    e.g.
    ```ts
    const audioPlayer = useAudioPlayer(); // May be null if `audioPlayer` not added to `<CogsConnectionProvider>`
    const isAudioPlaying = useIsAudioPlaying(audioPlayer);
    const audioClips = useAudioClips(audioPlayer);
    
    ```

--- src/hooks/useAudioClips.ts
+++ src/hooks/useAudioClips.ts
@@ -1,13 +1,13 @@
 import { AudioClip, AudioState, CogsAudioPlayer } from '@clockworkdog/cogs-client';
 import { useEffect, useState } from 'react';
 
-export default function useAudioClips(audioPlayer: CogsAudioPlayer): { [path: string]: AudioClip } {
+export default function useAudioClips(audioPlayer: CogsAudioPlayer | null): { [path: string]: AudioClip } {
   const [audioClips, setAudioClips] = useState<{ [path: string]: AudioClip }>({});
 
   useEffect(() => {
     const listener = (event: CustomEvent<AudioState>) => setAudioClips(event.detail.clips);
-    audioPlayer.addEventListener('state', listener);
-    return () => audioPlayer.removeEventListener('state', listener);
+    audioPlayer?.addEventListener('state', listener);
+    return () => audioPlayer?.removeEventListener('state', listener);
   }, [audioPlayer]);
 
   return audioClips;
