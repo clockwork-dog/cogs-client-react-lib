13602e267d85bb92659579823aebe78770e0e059
Chetan Padia <chetbox@users.noreply.github.com>
2024-02-22 10:42:23 +0000

    Add suport for HTTP2 assets (cogs-client v2.1)

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -26,7 +26,7 @@ export default function Images({
     <img
       className={className}
       key={index}
-      src={assetUrl(image.file)}
+      src={connection.getAssetUrl?.(image.file) ?? assetUrl(image.file)}
       alt={image.file}
       style={{
         objectFit: image.fit,
