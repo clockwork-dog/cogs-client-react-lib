692eb8ae2fe8c10a7b92843afdc082b97d2d8e20
Chetan Padia <chetbox@users.noreply.github.com>
2024-02-27 14:53:04 +0000

    `useIsAudioPlaying()` can use connection from `<CogsConnectionProvider>`

--- src/hooks/useIsAudioPlaying.ts
+++ src/hooks/useIsAudioPlaying.ts
@@ -1,8 +1,10 @@
 import { AudioState, CogsAudioPlayer } from '@clockworkdog/cogs-client';
 import { useEffect, useState } from 'react';
+import { useAudioPlayer } from '../providers/CogsConnectionProvider';
 
-export default function useIsAudioPlaying(audioPlayer: CogsAudioPlayer | null): boolean {
+export default function useIsAudioPlaying(customAudioPlayer?: CogsAudioPlayer | null): boolean {
   const [isAudioPlaying, setAudioPlaying] = useState(false);
+  const audioPlayer = useAudioPlayer(customAudioPlayer ?? undefined);
 
   useEffect(() => {
     const listener = (event: CustomEvent<AudioState>) => setAudioPlaying(event.detail.isPlaying);
